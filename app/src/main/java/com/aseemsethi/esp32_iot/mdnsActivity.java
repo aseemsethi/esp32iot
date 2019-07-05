55c45717735c4befc304619c56857e37cafaae6f
aseem sethi <aseemsethi@yahoo.com>
2019-07-05 20:38:43 +0530

    added clear config button

--- app/src/main/java/com/aseemsethi/esp32_iot/mdnsActivity.java
+++ app/src/main/java/com/aseemsethi/esp32_iot/mdnsActivity.java
@@ -8,6 +8,7 @@ import android.net.nsd.NsdServiceInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -24,10 +25,16 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.net.MalformedURLException;
 import java.net.Socket;
+import java.net.URL;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 
@@ -53,6 +60,8 @@ public class mdnsActivity extends AppCompatActivity {
     private static final String SERVICE_TYPE = "_http._tcp.";
     public String mServiceName = "ESP32";
     boolean disoveryStarted = false;
+    private static final int REQUEST_WIFI = 1;
+    private static final String KEY_RESPONSE_TEXT = "KEY_RESPONSE_TEXT";
 
     final String TAG = "ESP32IOT mDNS";
 
@@ -96,6 +105,17 @@ public class mdnsActivity extends AppCompatActivity {
                     }
                 };
                 new Thread(runnable).start();
+            }
+        });
+        Button clearB = (Button) findViewById(R.id.clearConfigB);
+        clearB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mRPiAddress.isEmpty()) return;
+                mAdapter.add("Clearing File Config..", Color.BLUE);
+                mRecyclerView.smoothScrollToPosition(mAdapter.getItemCount());
+                String uri = "http://" + mRPiAddress + "/clear";
+                startSendHttpRequestThread(uri);
             }
         });
         // Wait till thread above completes
@@ -324,4 +344,81 @@ public class mdnsActivity extends AppCompatActivity {
         finish();
     }
 
+    /* Start a thread to send http request to web server use HttpURLConnection object. */
+    private void startSendHttpRequestThread(final String reqUrl) {
+        Thread sendHttpRequestThread = new Thread(){
+            @Override public void run() {
+                // Maintain http url connection.
+                HttpURLConnection httpConn = null;
+                // Read text input stream.
+                InputStreamReader isReader = null;
+                // Read text into buffer.
+                BufferedReader bufReader = null;
+                // Save server response text.
+                StringBuffer readTextBuf = new StringBuffer();
+                try {
+                    // Create a URL object use page url.
+                    URL url = new URL(reqUrl);
+                    // Open http connection to web server.
+                    httpConn = (HttpURLConnection)url.openConnection();
+                    // Set http request method to get.
+                    httpConn.setRequestMethod("GET");
+                    // Set connection timeout and read timeout value.
+                    httpConn.setConnectTimeout(10000);
+                    httpConn.setReadTimeout(10000);
+                    // Get input stream from web url connection.
+                    InputStream inputStream = httpConn.getInputStream();
+                    // Create input stream reader based on url connection input stream.
+                    isReader = new InputStreamReader(inputStream);
+                    // Create buffered reader.
+                    bufReader = new BufferedReader(isReader);
+                    // Read line of text from server response.
+                    String line = bufReader.readLine();
+                    // Loop while return line is not null.
+                    while(line != null){
+                        // Append the text to string buffer.
+                        readTextBuf.append(line);
+                        // Continue to read text line.
+                        line = bufReader.readLine();
+                    }
+                    // Send message to main thread to update response text in TextView after read all.
+                    Message message = new Message();
+                    // Set message type.
+                    message.what = REQUEST_WIFI;
+                    // Create a bundle object.
+                    Bundle bundle = new Bundle();
+                    // Put response text in the bundle with the special key.
+                    bundle.putString(KEY_RESPONSE_TEXT, readTextBuf.toString());
+                    // Set bundle data in message.
+                    message.setData(bundle);
+                    Log.d(TAG, "Recvd HTTP Msg: " + readTextBuf.toString());
+                    // Send message to main thread Handler to process.
+                    //uiUpdater.sendMessage(message);
+                } catch(MalformedURLException ex) {
+                    Log.e(TAG, ex.getMessage(), ex);
+                } catch(IOException ex) {
+                    Log.e(TAG, ex.getMessage(), ex);
+                } finally {
+                    try {
+                        if (bufReader != null) {
+                            bufReader.close();
+                            bufReader = null;
+                        }
+                        if (isReader != null) {
+                            isReader.close();
+                            isReader = null;
+                        }
+                        if (httpConn != null) {
+                            httpConn.disconnect();
+                            httpConn = null;
+                        }
+                    } catch (IOException ex) {
+                        Log.e(TAG, ex.getMessage(), ex);
+                    }
+                }
+            }
+        };
+        // Start the child thread to request web page.
+        sendHttpRequestThread.start();
+    }
 }
